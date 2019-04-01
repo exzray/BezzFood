08d26591c9486a3195daed435d7182edd98d0c3e
pytho <pythorevolution@gmail.com>
2019-04-01 20:15:20 +0800

    commit

--- app/src/main/java/com/example/bezzfood/MenuListActivity.java
+++ app/src/main/java/com/example/bezzfood/MenuListActivity.java
@@ -2,12 +2,36 @@ package com.example.bezzfood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.example.bezzfood.utility.Data;
 
 public class MenuListActivity extends AppCompatActivity {
+
+    private String md_uid;
+    private String md_name;
+
+    private Toolbar mv_toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_list);
+
+        md_uid = getIntent().getStringExtra(Data.EXTRA_STRING_UID);
+        md_name = getIntent().getStringExtra(Data.EXTRA_STRING_NAME);
+
+        mv_toolbar = findViewById(R.id.toolbar);
+
+        initUI();
+    }
+
+    private void initUI(){
+        setSupportActionBar(mv_toolbar);
+        setTitle(md_name);
+
+        Toast.makeText(this, "Uid: " + md_uid, Toast.LENGTH_SHORT).show();
     }
 }
